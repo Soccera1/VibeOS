1bfbd6cc0f03c589230d690ef479b517fef9e4a3
lily <gentop>
2026-01-14 22:24:23 +1100

    Cleanup repository: remove build artifacts from tracking

--- busybox-1.36.1/applets/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_applets/built-in.o :=  gcc -m32 -nostdlib -nostdlib -nostdlib -T ../user.ld ../user/start.o ../libc.o -r -o applets/built-in.o applets/applets.o
--- busybox-1.36.1/coreutils/.lib.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_coreutils/lib.a := rm -f coreutils/lib.a; ar  rcs coreutils/lib.a coreutils/cat.o coreutils/cp.o coreutils/echo.o coreutils/false.o coreutils/ls.o coreutils/mkdir.o coreutils/mv.o coreutils/pwd.o coreutils/rm.o coreutils/sleep.o coreutils/true.o
